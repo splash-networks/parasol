6d8fc34aecb5d7cc7cde71e8b2fbc346125a8a28
Nasir Hafeez <38311694+nasirhafeez@users.noreply.github.com>
2024-06-12 14:31:09 +0500

    plasma

--- public/connect.php
+++ public/connect.php
@@ -3,51 +3,90 @@
 require 'header.php';
 include 'config.php';
 
+function encode_password($plain, $challenge, $secret) {
+  if ((strlen($challenge) % 2) != 0 ||
+    strlen($challenge) == 0)
+    return FALSE;
+
+  $hexchall = hex2bin($challenge);
+  if ($hexchall === FALSE)
+    return FALSE;
+
+  if (strlen($secret) > 0) {
+    $crypt_secret = md5($hexchall . $secret, TRUE);
+    $len_secret = 16;
+  } else {
+    $crypt_secret = $hexchall;
+    $len_secret = strlen($hexchall);
+  }
+
+  /* simulate C style \0 terminated string */
+  $plain .= "\x00";
+  $crypted = '';
+  for ($i = 0; $i < strlen($plain); $i++)
+    $crypted .= $plain[$i] ^ $crypt_secret[$i % $len_secret];
+
+  $extra_bytes = 0;//rand(0, 16);
+  for ($i = 0; $i < $extra_bytes; $i++)
+    $crypted .= chr(rand(0, 255));
+
+  return bin2hex($crypted);
+}
+
+$uam_secret = $_SERVER['UAM_SECRET'];
+$redirect_url = $_SERVER['REDIRECT_URL'];
 $mac = $_SESSION["mac"];
-$ip = $_SESSION["ip"];
-$link_login = $_SESSION["link-login"];
-$link_login_only = $_SESSION["link-login-only"];
-$linkorig = "https://www.google.com";
+$user_type = $_SESSION["user_type"];
+$last_updated = date("Y-m-d H:i:s");
 
-$username = "test";
-$password = "test123";
+if ($user_type == "new") {
+  $fname = $_POST['fname'];
+  $lname = $_POST['lname'];
+  $email = $_POST['email'];
 
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$email = $_POST['email'];
-
-if ($_SESSION["user_type"] == "new") {
-    mysqli_query($con, "
+  mysqli_query($con, "
     CREATE TABLE IF NOT EXISTS `$table_name` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `firstname` varchar(45) NOT NULL,
     `lastname` varchar(45) NOT NULL,
     `email` varchar(45) NOT NULL,
     `mac` varchar(45) NOT NULL,
-    `ip` varchar(45) NOT NULL,
     `last_updated` varchar(45) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (mac)
     )");
 
-    mysqli_query($con,"INSERT INTO `$table_name` (firstname, lastname, email, mac, ip, last_updated) VALUES ('$fname', '$lname', '$email', '$mac', '$ip', NOW())");
+  mysqli_query($con,"INSERT INTO `$table_name` (firstname, lastname, email, mac, last_updated) VALUES ('$fname', '$lname', '$email', '$mac', '$last_updated')");
 }
 
-mysqli_close($con);
+$username = $mac;
+$password = $mac;
+$uamip = $_SESSION["uamip"];
+$uamport = $_SESSION["uamport"];
+$challenge = $_SESSION["challenge"];
+
+$encoded_password = encode_password($password, $challenge, $uam_secret);
+
+$uam_redirect_url = "http://$uamip:$uamport/logon?" .
+  "username=" . urlencode($username) .
+  "&response=" . urlencode($encoded_password);
 
 ?>
 <!DOCTYPE HTML>
 <html>
+
 <head>
     <meta charset="utf-8">
-    <title>
-      <?php echo htmlspecialchars($business_name); ?> WiFi</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <link rel="stylesheet" href="assets/styles/bulma.min.css"/>
-    <link rel="stylesheet" href="vendor/fortawesome/font-awesome/css/all.css"/>
-    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-32x32.png" sizes="32x32"/>
-    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16"/>
-    <link rel="stylesheet" href="assets/styles/style.css"/>
+    <title><?php echo htmlspecialchars($business_name); ?> WiFi</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" href="assets/styles/bulma.min.css" />
+    <link rel="stylesheet" href="vendor/fortawesome/font-awesome/css/all.css" />
+    <meta http-equiv="refresh" content="2;url=<?php echo htmlspecialchars($uam_redirect_url); ?>" />
+    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
+    <link rel="stylesheet" href="assets/styles/style.css" />
 </head>
+
 <body>
 <div class="page">
 
@@ -60,32 +99,11 @@ mysqli_close($con);
 
     <div class="main">
         <seection class="section">
-            <div class="container">
-                <div id="margin_zero" class="content has-text-centered is-size-6">Please wait, you are being</div>
-                <div id="margin_zero" class="content has-text-centered is-size-6">authorized on the network</div>
-            </div>
+            <div id="margin_zero" class="content has-text-centered is-size-6">Thanks, you are being</div>
+            <div id="margin_zero" class="content has-text-centered is-size-6">authorized on WiFi</div>
         </seection>
     </div>
 
 </div>
-
-<script type="text/javascript">
-    function formAutoSubmit () {
-        var frm = document.getElementById("login");
-        document.getElementById("login").submit();
-        frm.submit();
-    }
-    // window.onload = formAutoSubmit;
-    window.onload = setTimeout(formAutoSubmit, 2500);
-
-</script>
-
-<form id="login" method="post" action="<?php echo $link_login_only; ?>">
-    <input name="dst" type="hidden" value="<?php echo $linkorig; ?>" />
-    <input name="popup" type="hidden" value="false" />
-    <input name="username" type="hidden" value="<?php echo $username; ?>"/>
-    <input name="password" type="hidden" value="<?php echo $password; ?>"/>
-</form>
-
 </body>
 </html>
--- public/index.php
+++ public/index.php
@@ -3,15 +3,16 @@
 require 'header.php';
 include 'config.php';
 
-$_SESSION["mac"] = $_POST['mac'];
-$_SESSION["ip"] = $_POST['ip'];
-$_SESSION["link-login"] = $_POST['link-login'];
-$_SESSION["link-login-only"] = $_POST['link-login-only'];
-
+$_SESSION["mac"] = $_GET["mac"];
+$_SESSION["challenge"] = $_GET["challenge"];
+$_SESSION["uamip"] = $_GET["uamip"];
+$_SESSION["uamport"] = $_GET["uamport"];
+$_SESSION["userurl"] = $_GET["userurl"];
 $_SESSION["user_type"] = "new";
 
-# Checking DB to see if user exists or not.
+$res = $_GET["res"];
 
+# Checking DB to see if user exists or not.
 mysqli_report(MYSQLI_REPORT_OFF);
 $result = mysqli_query($con, "SELECT * FROM `$table_name` WHERE mac='$_SESSION[mac]'");
 
@@ -21,7 +22,7 @@ if ($result->num_rows >= 1) {
   mysqli_close($con);
 
   $_SESSION["user_type"] = "repeat";
-  header("Location: welcome.php");
+  header("Location: connect.php");
 } else {
   mysqli_close($con);
 }
@@ -34,71 +35,92 @@ if ($result->num_rows >= 1) {
     <meta charset="utf-8">
     <title>
       <?php echo htmlspecialchars($business_name); ?> WiFi</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-    <link rel="stylesheet" href="assets/styles/bulma.min.css"/>
-    <link rel="stylesheet" href="vendor/fortawesome/font-awesome/css/all.css"/>
-    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-32x32.png" sizes="32x32"/>
-    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16"/>
-    <link rel="stylesheet" href="assets/styles/style.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" href="assets/styles/bulma.min.css" />
+    <link rel="stylesheet" href="vendor/fortawesome/font-awesome/css/all.css" />
+    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="assets/images/favicomatic/favicon-16x16.png" sizes="16x16" />
+    <link rel="stylesheet" href="assets/styles/style.css" />
 </head>
 
 <body>
-<div class="page">
 
-    <div class="head">
-        <br>
-        <figure id="logo">
-            <img src="assets/images/logo.png">
-        </figure>
-    </div>
+<?php
+if ($res === "notyet") {
+  ?>
 
-    <div class="main">
-        <section class="section">
-            <div class="container">
-                <div id="contact_form" class="content is-size-5 has-text-centered has-text-weight-bold">Enter your details
+    <div class="page">
+
+        <div class="head">
+            <br>
+            <figure id="logo">
+                <img src="assets/images/logo.png">
+            </figure>
+        </div>
+
+        <div class="main">
+            <section class="section">
+                <div class="container">
+                    <div id="login" class="content is-size-5 has-text-centered has-text-weight-bold">Enter your details</div>
+                    <form method="post" action="connect.php">
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input" type="text" id="form_font" name="fname" placeholder="First Name" required>
+                                <span class="icon is-small is-left">
+                        <i class="fas fa-user"></i>
+                    </span>
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input" type="text" id="form_font" name="lname" placeholder="Last Name" required>
+                                <span class="icon is-small is-left">
+                    <i class="fas fa-user"></i>
+                </span>
+                            </div>
+                        </div>
+
+                        <div class="field">
+                            <div class="control has-icons-left">
+                                <input class="input" type="email" id="form_font" name="email" placeholder="Email" required>
+                                <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                    </span>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="columns is-centered is-mobile">
+                            <div class="control">
+                                <label class="checkbox">
+                                    <input type="checkbox" required>
+                                    I agree to the <a href="policy.php">Terms of Use</a>
+                                </label>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="buttons is-centered">
+                            <button class="button is-link">Connect</button>
+                        </div>
+                    </form>
                 </div>
-                <form method="post" action="connect.php">
-                    <div class="field">
-                        <div class="control has-icons-left">
-                            <input class="input" type="text" id="form_font" name="fname" placeholder="First Name" required>
-                            <span class="icon is-small is-left">
-                                <i class="fas fa-user"></i>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="control has-icons-left">
-                            <input class="input" type="text" id="form_font" name="lname" placeholder="Last Name" required>
-                            <span class="icon is-small is-left">
-                                <i class="fas fa-user"></i>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="field">
-                        <div class="control has-icons-left">
-                            <input class="input" type="email" id="form_font" name="email" placeholder="Email" required>
-                            <span class="icon is-small is-left">
-                                <i class="fas fa-envelope"></i>
-                            </span>
-                        </div>
-                    </div>
-                    <br>
-                    <div class="columns is-centered is-mobile">
-                        <div class="control">
-                            <label class="checkbox">
-                                <input type="checkbox" required>
-                                I agree to the <a href="policy.php">Terms of Use</a>
-                            </label>
-                        </div>
-                    </div>
-                    <br>
-                    <div class="buttons is-centered">
-                        <button class="button is-link">Connect</button>
-                    </div>
-                </form>
-            </div>
-        </section>
+                <br>
+            </section>
+        </div>
     </div>
-</div>
+  <?php
+} else if ($res === "success") {
+  header("Location: $redirect_url");
+} else if ($res === "failed") {
+  echo "<h2>Sorry, failed to authenticate</h2>";
+} else if ($res === "logoff") {
+  echo "<h2>Logging off...</h2>";
+} else if ($res === "already") {
+  header("Location: $redirect_url");
+} else {
+  echo "<h2>Error: Permission Denied</h2>";
+}
+?>
+
 </body>
 </html>
